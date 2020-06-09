1c19a6e44cffdbb46f8e418883c6f23dbcf32110
Sebastian Glaß <sebastian.glass15@gmail.com>
2020-06-09 08:30:09 +0200

    PoC: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.spec.json'
+        }
+    },
+    roots: [
+        "<rootDir>/src"
+    ],
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    }
 };
